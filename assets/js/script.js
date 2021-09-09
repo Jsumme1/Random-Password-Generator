00d7b1db19589acaa359a7ddb6806cac708cf508
Jsummers1 <julie.summers2000@gmail.com>
2021-09-09 17:45:24 -0500

    initial characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
 // Assignment code here
+// User input variables
+var enter;
+var confirmNumber;
+var confimCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+//Special Characters
+characters =
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "~"
+];
+
+//Number Characters:
+number=[,1,2,3,4,5,6,7,8,9];
+
+//Letters
+alpha=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Uppercase conversion
+space=[];
+
+//converts letters to uppercase
+var toUpper = function(x){
+    return x.toUpperCase();
+};
+
+//create a variable for uppercase conversion
+
+alpha2= alpha.map(toUpper);
+
+var get= document.querySelector("#generate");    
+
+get.addEventListener("click", function () {
+    ps= generatePassword();
+    document.getElementById("password").placeholder = ps;
+
+});
+
+
+
+
 
 
 // Get references to the #generate element
